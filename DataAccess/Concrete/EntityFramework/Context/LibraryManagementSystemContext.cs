330f21c4cd0a7d0f5d8cc2b23c3d2a8a1c5c6367
Emirhan SEN <emirrssen@gmail.com>
2023-04-02 16:33:37 +0300

    All data access configurations done.

---
+++ DataAccess/Concrete/EntityFramework/Context/LibraryManagementSystemContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entity.Concrete;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class LibraryManagementSystemContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Borrow> Borrows { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=EMIRRSSEN;Database=LibraryManagementSystem;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+    }
+}
