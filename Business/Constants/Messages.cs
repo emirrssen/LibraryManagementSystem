0ef94b66df0d7556cd9882e40440b2eb5bbfa53f
Emirhan SEN <emirrssen@gmail.com>
2023-04-02 19:51:26 +0300

    Return messages wrote.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,74 +8,74 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        internal static string AuthorAdded;
-        internal static string AuthorDeleted;
-        internal static string AuthorUpdated;
-        internal static string AuthorsListed;
-        internal static string AuthorListed;
+        public static string AuthorAdded = "Author added successfully!";
+        public static string AuthorDeleted = "Author deleted successfully!";
+        public static string AuthorUpdated = "Author updated successfully!";
+        public static string AuthorsListed = "Authors listed successfully!";
+        public static string AuthorListed = "Author listed successfully!";
 
-        internal static string BookAuthorAdded;
-        internal static string BookAuthorDeleted;
-        internal static string BookAuthorUpdated;
-        internal static string BookAuthorsListed;
-        internal static string BookAuthorListed;
+        public static string BookAuthorAdded = "BookAuthor added successfully!";
+        public static string BookAuthorDeleted = "BookAuthor deleted successfully!";
+        public static string BookAuthorUpdated = "BookAuthor updated successfully!";
+        public static string BookAuthorsListed = "BookAuthors listed successfully!";
+        public static string BookAuthorListed = "BookAuthor listed successfully!";
 
-        internal static string BookCategoryAdded;
-        internal static string BookCategoryDeleted;
-        public static string BookCategoryUpdated;
-        public static string BookCategoriesListed;
-        public static string BookCategoryListed;
+        public static string BookCategoryAdded = "BookCategory added successfully!";
+        public static string BookCategoryDeleted = "BookCategory deleted successfully!";
+        public static string BookCategoryUpdated = "BookCategory updated successfully!";
+        public static string BookCategoriesListed = "BookCategories listed successfully!";
+        public static string BookCategoryListed = "BookCategory listed successfully!";
 
-        internal static string BookAdded;
-        internal static string BookDeleted;
-        internal static string BookUpdated;
-        internal static string BooksListed;
-        internal static string BookListed;
+        public static string BookAdded = "Book added successfully!";
+        public static string BookDeleted = "Book deleted successfully!";
+        public static string BookUpdated = "Book updated successfully!";
+        public static string BooksListed = "Books listed successfully!";
+        public static string BookListed = "Book listed successfully!";
 
-        internal static string BorrowAdded;
-        internal static string BorrowDeleted;
-        internal static string BorrowUpdated;
-        internal static string BorrowsListed;
-        internal static string BorrowListed;
+        public static string BorrowAdded = "Borrow added successfully!";
+        public static string BorrowDeleted = "Borrow deleted successfully!";
+        public static string BorrowUpdated = "Borrow updated successfully!";
+        public static string BorrowsListed = "Borrows listed successfully!";
+        public static string BorrowListed = "Borrow listed successfully!";
 
-        internal static string CategoryAdded;
-        internal static string CategoryDeleted;
-        internal static string CategoryUpdated;
-        internal static string CategoriesListed;
-        internal static string CategoryListed;
+        public static string CategoryAdded = "Category added successfully!";
+        public static string CategoryDeleted = "Category deleted successfully!";
+        public static string CategoryUpdated = "Category updted successfully!";
+        public static string CategoriesListed = "Categories listed successfully!";
+        public static string CategoryListed = "Category listed successfully!";
 
-        internal static string DepartmentAdded;
-        internal static string DepartmentDeleted;
-        internal static string DepartmentUpdated;
-        internal static string DepartmentsListed;
-        internal static string DepartmentListed;
+        public static string DepartmentAdded = "Department added successfully!";
+        public static string DepartmentDeleted = "Department deleted successfully!";
+        public static string DepartmentUpdated = "Department updated successfully!";
+        public static string DepartmentsListed = "Departments listed successfully!";
+        public static string DepartmentListed = "Department listed successfully!";
 
-        internal static string OperationClaimAdded;
-        internal static string OperationClaimDeleted;
-        internal static string OperationClaimUpdated;
-        internal static string OperationClaimsListed;
-        internal static string OperationClaimListed;
+        public static string OperationClaimAdded = "OperationClaim added successfully!";
+        public static string OperationClaimDeleted = "OperationClaim deleted successfully!";
+        public static string OperationClaimUpdated = "OperationClaim updated successfully!";
+        public static string OperationClaimsListed = "OperationClaims listed successfully!";
+        public static string OperationClaimListed = "OperationClaim listed successfully!";
 
-        internal static string PersonelAdded;
-        internal static string PersonelDeleted;
-        internal static string PersonelUpdated;
-        internal static string PersonelsListed;
-        internal static string PersonelListed;
+        public static string PersonelAdded = "Personel added successfully!";
+        public static string PersonelDeleted = "Personel deleted successfully!";
+        public static string PersonelUpdated = "Personel updated successfully!";
+        public static string PersonelsListed = "Personels listed successfully!";
+        public static string PersonelListed = "Personel listed successfully!";
 
-        internal static string PersonAdded;
-        internal static string PersonDeleted;
-        internal static string PersonUpdated;
-        internal static string PersonsListed;
-        internal static string PersonListed;
+        public static string PersonAdded = "Person added successfully!";
+        public static string PersonDeleted = "Person deleted successfully!";
+        public static string PersonUpdated = "Person updated successfully!";
+        public static string PersonsListed = "Persons listed successfully!";
+        public static string PersonListed = "Person listed successfully!";
 
-        internal static string StudentAdded;
-        internal static string StudentDeleted;
-        internal static string StudentUpdated;
-        internal static string StudentsListed;
-        internal static string StudentListed;
+        public static string StudentAdded = "Student added successfully!";
+        public static string StudentDeleted = "Student deleted successfully!";
+        public static string StudentUpdated = "Student updated successfully!";
+        public static string StudentsListed = "Students listed successfully!";
+        public static string StudentListed = "Student listed successfully!";
 
-        internal static string UserAdded;
-        internal static string UserListed;
-        internal static string ClaimAdded;
+        public static string UserAdded = "User added successfully!";
+        public static string UserListed = "User listed successfully!";
+        public static string ClaimAdded = "Claim added successfully!";
     }
 }
